fed548bb68084b07bcde07d6c0ff63a92884c8f4
Hyunjun Joo <joohjun0315@gmail.com>
2026-09-18 00:41:04 +0900

    Create post.py

---
+++ post.py
@@ -0,0 +1,30 @@
+import os
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+import requests
+
+
+ACCESS_TOKEN = os.environ["THREADS_ACCESS_TOKEN"]
+
+
+# 게시할 글
+text = "Squak!"
+
+response = requests.post(
+    "https://graph.threads.net/v1.0/me/threads",
+    headers={
+        "Authorization": f"Bearer {ACCESS_TOKEN}"
+    },
+    data={
+        "media_type": "TEXT",
+        "text": text,
+        "auto_publish_text": "true",
+    },
+    timeout=30,
+)
+
+response.raise_for_status()
+
+print("게시 성공")
+print(response.json())
